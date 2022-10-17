8a2eee4dbd54e54256a8707f392f26c9c45e8e37
jmariotto93 <josemarioto@gmail.com>
2022-10-17 16:09:06 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("BA", "Bahia");
+estados.Add("MG", "Minas Gerais");
+estados.Add("RJ", "Rio de Janeiro");
+
+foreach (var item in estados)
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+
+Console.WriteLine("----------------------------------------");
+Console.WriteLine($"Removando o estado Bahia - {estados.Remove("BA")}");
+Console.WriteLine("----------------------------------------");
+
+foreach (var item in estados)
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+
+
+Console.WriteLine("---------------------------------");
+string chave = "BA";
+Console.WriteLine($"Verificando o elemento: {chave}");
+
+if (estados.ContainsKey(chave))
+{
+    Console.WriteLine($"Valor existente {chave}");
+}
+else
+{
+    Console.WriteLine($"Valor não existe. É seguro adicionar a chave: {chave}");
+}
